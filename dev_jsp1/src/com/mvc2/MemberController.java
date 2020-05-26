efc95f98dde30efe297e4a9107ce4f9284328d0a
rhrlwlfk <tkdguszz123@naver.com>
2020-05-26 15:28:40 +0900

    0526

--- dev_jsp1/src/com/mvc2/MemberController.java
+++ dev_jsp1/src/com/mvc2/MemberController.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 
-
 public class MemberController implements Controller{
 	Logger logger = Logger.getLogger(MemberController.class);
 	String crud = null;
@@ -17,7 +16,12 @@ public class MemberController implements Controller{
 	@Override
 	public String process(HttpServletRequest req, HttpServletResponse res) throws ServletException {
 		logger.info("process 호출성공");//로그 확인 
-		
+		//여러가지 업무가 나눠질수 있으니까 
+		if("login".equals(crud)) {
+			//memberInsert 회원가입 
+		}else if("memberInsert".equals(crud)) {
+			
+		}
 		return "forward:memberList.jsp";
 	}
 
---
+++ dev_jsp1/src/com/mvc2/MemberDao.java
@@ -0,0 +1,8 @@
+package com.mvc2;
+
+import org.apache.log4j.Logger;
+
+public class MemberDao {
+	Logger logger = Logger.getLogger(MemberDao.class);
+	
+}
